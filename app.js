d77690d2a0f146ef67320bf0494ec17eccd74791
yuweo09 <Karlwatson09@gmail.com>
2016-09-12 21:17:51 -0700

    comments out

--- app.js
+++ app.js
@@ -10,18 +10,13 @@ if(localStorage.length > 0){
   var chec = document.createElement('INPUT')
   chec.setAttribute('type', 'checkbox');
 
-
+//local storage
   var lab;
-    var temp;
-  var la;
+
   for (var i = 0; i < localStorage.length; i++) {
     lab = localStorage.getItem(i);
     if(lab.includes('strike'))
     {
-      // temp = lab.split("<strike>");
-      //  la  = temp[1].split("</strike>")
-
-      // let childNode = document.getElementById('todoList').childNodes[childNodeIndex].innerHTML;
 
       labe.innerHTML = lab;
       chec.setAttribute("checked", "true");
@@ -34,8 +29,6 @@ if(localStorage.length > 0){
       }
       $(document.getElementById('todoList').childNodes[childNodeIndex]).attr('id', id);
 
-      temp=[];
-      la=[];
     }else{
       labe.innerHTML = lab;
       document.getElementById('todoList').appendChild(chec);
@@ -50,10 +43,6 @@ if(localStorage.length > 0){
     }
       id++;
   }
-  // lab='';
-  // labe='';
-  // b='';
-  // chec='';
 
 }
 // (function() {
@@ -71,45 +60,26 @@ var isList = [];
     //  console.log('This isn\'t the right type of element clicked');
      return false;
    }
-  //  console.log('It\'s the right type of element!');
-// console.log(event);
+
   var $s = $(event.target);
   console.log(event.target.id);
- //  console.log($s[0].checked);
-  // for(var i = 0; i < $s.length; i++){
-    // console.log($s[i]);
-    //  console.log($s[i].checked);
-  //  if($s[i].checked === true){
-  // console.log($s.innerHTML);
      $s[0].innerHTML = $s[0].innerHTML.strike();
      let prev =  $s[0].previousSibling.checked = true;
      console.log(event.target);
      localStorage.setItem(event.target.id, document.getElementById(event.target.id).innerHTML)
-  //  }
-  // }
- //  $s.get(0).innerHTML = $s.get(0).innerHTML.strike();
 }
 var $list = $('#todoList').on('click', strike);
 
 function todoList() {
 
-// store user value
+//store user value
 var br = document.createElement('BR')
 
 var check = document.createElement('INPUT')
 check.setAttribute('type', 'checkbox');
-
-// check.addEventListener('onchange', strike);
 var label = document.createElement('LABEL')
 var item    = document.getElementById('todoInput').value
-// create a text node from the user input
-// var text    = document.createTextNode(item)
-// create a li tag
-// var newItem = document.createElement('li')
-// add the user input to the li tag
 label.innerHTML = item;
-// newItem.appendChild(text)
-// append the li to the html todoList id tag
 document.getElementById('todoList').appendChild(check);
 document.getElementById('todoList').appendChild(label);
 document.getElementById('todoList').appendChild(br);
@@ -129,22 +99,14 @@ id++;
     $('#qoute').get(0).innerHTML= "";
     let mood = event.target.value;
     let length = quotes[mood].length;
-    // console.log(mood);
-    // console.log(quotes[mood].length);
-    // var rand = myArray[Math.floor(Math.random() * myArray.length)];
     $('#qoute').append(quotes[mood][Math.floor(Math.random() * length)]);
     // console.log(quotes[mood][Math.floor(Math.random() * length)]);
     var xhr = $.get(`http://api.giphy.com/v1/gifs/search?q=${mood}&api_key=dc6zaTOxFJmzC`);
     xhr.done(function(data) {
-
       imageTabsDiv = $('#images').get(0);
-       //
-      //  console.log("success got data", data);
-      //  console.log(data.data.length);
-      // console.log(data);
          for(var i = 0; i < data.data.length; i++)
          {
-            //  console.log(data.data[i].images.downsized.url);
+            
              var img = document.createElement("img");
              img.src = data.data[i].images.fixed_height.url ;
              imageTabsDiv.appendChild(img);
