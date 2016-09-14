2f7f9d3bff8024b35a6464d13c153f98d1c1743a
yuweo09 <Karlwatson09@gmail.com>
2016-09-14 00:02:45 -0700

    el

--- app.js
+++ app.js
@@ -1,120 +1,124 @@
+(function() {
+  'use strict';
+  let id = 0;
 
+  if (localStorage.length > 0) {
+    id = 0;
 
-var id = 0;
-if(localStorage.length > 0){
-  // var labels=[];
-  id=0;
-  var labe = document.createElement('LABEL')
-  var b = document.createElement('BR')
+    const labe = document.createElement('LABEL');
 
-  var chec = document.createElement('INPUT')
-  chec.setAttribute('type', 'checkbox');
+    const bre = document.createElement('BR');
 
-//local storage
-  var lab;
+    const chec = document.createElement('INPUT');
 
-  for (var i = 0; i < localStorage.length; i++) {
-    lab = localStorage.getItem(i);
-    if(lab.includes('strike'))
-    {
+    chec.setAttribute('type', 'checkbox');
+    let lab;
 
-      labe.innerHTML = lab;
-      chec.setAttribute("checked", "true");
-      document.getElementById('todoList').appendChild(chec);
-      document.getElementById('todoList').appendChild(labe);
-      document.getElementById('todoList').appendChild(b);
-      let childNodeIndex = 1;
-      if (id !== 0) {
-        childNodeIndex = (id*3)+1;
+    let tdl = $(document.getElementById('todoList'));
+
+    for (let i = 0; i < localStorage.length; i++) {
+      lab = localStorage.getItem(i);
+      if (lab.includes('strike')) {
+        labe.innerHTML = lab;
+        chec.setAttribute('checked', 'true');
+        document.getElementById('todoList').appendChild(chec);
+        document.getElementById('todoList').appendChild(labe);
+        document.getElementById('todoList').appendChild(bre);
+        let childNodeIndex = 1;
+
+        if (id !== 0) {
+          childNodeIndex = (id * 3) + 1;
+        }
+        tdl = $(document.getElementById('todoList'));
+
+        tdl.childNodes[childNodeIndex].attr('id', id);
       }
-      $(document.getElementById('todoList').childNodes[childNodeIndex]).attr('id', id);
+      else {
+        labe.innerHTML = lab;
+        document.getElementById('todoList').appendChild(chec);
+        document.getElementById('todoList').appendChild(labe);
+        document.getElementById('todoList').appendChild(bre);
 
-    }else{
-      labe.innerHTML = lab;
-      document.getElementById('todoList').appendChild(chec);
-      document.getElementById('todoList').appendChild(labe);
-      document.getElementById('todoList').appendChild(b);
-      let childNodeIndex = 1;
-      if (id !== 0) {
-        childNodeIndex = (id*3)+1;
+        let childNodeIndex = 1;
+
+        if (id !== 0) {
+          childNodeIndex = (id * 3) + 1;
+        }
+        tdl = $(document.getElementById('todoList'));
+
+        tdl.childNodes[childNodeIndex].attr('id', id);
       }
-      $(document.getElementById('todoList').childNodes[childNodeIndex]).attr('id', id);
-
-    }
       id++;
+    }
   }
-
-}
-// (function() {
-var isList = [];
-
-  var quotes = {
-    Happy: ['Be content with what you have; rejoice in the way things are. When you realize there is nothing lacking, the whole world belongs to you.' ],
-    Sad: ['Happiness depends upon ourselves','Things turn out best for people who make the best out of the way things turn out.','The greatest pleasure in life is doing what people say you cannot do.'],
-    Motivation: ['It always seems impossible until its done.','Good, better, best. Never let it rest. \'Til your good is better and your better is best.','Always do your best. What you plant now, you will harvest later.'],
-    Funny: ['A woman\'s mind is cleaner than a man\'s: She changes it more often.','I believe that if life gives you lemons, you should make lemonade... And try to find somebody whose life has given them vodka, and have a party.','Get your facts first, then you can distort them as you please.'],
+  const quotes = {
+    Happy: ['Be content with what you have; rejoice in the way things are. When you realize there is nothing lacking, the whole world belongs to you.'],
+    Sad: ['Happiness depends upon ourselves', 'Things turn out best for people who make the best out of the way things turn out.', 'The greatest pleasure in lif e is doing what people say you cannot do.'],
+    Motivation: ['It always seems impossible until its done.', 'Good, better, best. Never let it rest. \'Til your good is better and your better is best.', 'Always do your best. What you plant now, you will harvest later.'],
+    Funny: ['A woman\'s mind is cleaner than a man\'s: She changes it more often.', 'I believe that if  lif e gives you lemons, you should make lemonade... And try to find somebody whose lif e has given them vodka, and have a party.', 'Get your facts first, then you can distort them as you please.']
   };
- $(".button-collapse").sideNav();
- function strike(event) {
-   if((event.target.tagName !== 'LABEL') && (event.target.tagName !== 'INPUT')){
-    //  console.log('This isn\'t the right type of element clicked');
-     return false;
-   }
 
-  var $s = $(event.target);
-  console.log(event.target.id);
-     $s[0].innerHTML = $s[0].innerHTML.strike();
-     let prev =  $s[0].previousSibling.checked = true;
-     console.log(event.target);
-     localStorage.setItem(event.target.id, document.getElementById(event.target.id).innerHTML)
-}
-var $list = $('#todoList').on('click', strike);
+  $('.button-collapse').sideNav();
 
-function todoList() {
+  const strike = function(event) {
+    if ((event.target.tagName !== 'LABEL') && (event.target.tagName !== 'INPUT')) {
+      return false;
+    }
+    const $lbl = $(event.target);
 
-//store user value
-var br = document.createElement('BR')
+    $lbl[0].innerHTML = $lbl[0].innerHTML.strike();
+    $lbl[0].previousSibling.checked = true;
 
-var check = document.createElement('INPUT')
-check.setAttribute('type', 'checkbox');
-var label = document.createElement('LABEL')
-var item    = document.getElementById('todoInput').value
-label.innerHTML = item;
-document.getElementById('todoList').appendChild(check);
-document.getElementById('todoList').appendChild(label);
-document.getElementById('todoList').appendChild(br);
-let childNodeIndex = 1;
-if (id !== 0) {
-  childNodeIndex = (id*3)+1;
-}
-$(document.getElementById('todoList').childNodes[childNodeIndex]).attr('id', id);
-// let childNode = document.getElementById('todoList').childNodes[childNodeIndex].innerHTML;
-let childNode = $(`#${id}`).text();
-localStorage.setItem(id,childNode);
-console.log(localStorage);
-id++;
-}
-  $('#mood').change(function functionName(event) {
-    var imageTabsDiv = $('#images').get(0).innerHTML='';
-    $('#qoute').get(0).innerHTML= "";
-    let mood = event.target.value;
-    let length = quotes[mood].length;
+    localStorage.setItem(event.target.id, document.getElementById(event.target.id).innerHTML);
+  };
+  $('#todoList').on('click', strike);
+
+  function todoList() {
+    const br = document.createElement('BR');
+
+    const check = document.createElement('INPUT');
+
+    check.setAttribute('type', 'checkbox');
+
+    const label = document.createElement('LABEL');
+
+    const item = document.getElementById('todoInput').value;
+
+    label.innerHTML = item;
+    document.getElementById('todoList').appendChild(check);
+    document.getElementById('todoList').appendChild(label);
+    document.getElementById('todoList').appendChild(br);
+    let childNodeIndex = 1;
+
+    if (id !== 0) {
+      childNodeIndex = (id * 3) + 1;
+    }
+    $(document.getElementById('todoList').childNodes[childNodeIndex]).attr('id', id);
+    const childNode = $(`#${id}`).text();
+
+    localStorage.setItem(id, childNode);
+    id++;
+  }
+  $('#mood').change(function (event) {
+    let imageTabsDiv = $('#images').get(0).innerHTML = '';
+
+    $('#qoute').get(0).innerHTML = '';
+
+    const mood = event.target.value;
+
+    const length = quotes[mood].length;
+
     $('#qoute').append(quotes[mood][Math.floor(Math.random() * length)]);
-    // console.log(quotes[mood][Math.floor(Math.random() * length)]);
-    var xhr = $.get(`http://api.giphy.com/v1/gifs/search?q=${mood}&api_key=dc6zaTOxFJmzC`);
+    const xhr = $.get(`http://api.giphy.com/v1/gifs/search?q=${mood}&api_key=dc6zaTOxFJmzC`);
+
     xhr.done(function(data) {
       imageTabsDiv = $('#images').get(0);
-         for(var i = 0; i < data.data.length; i++)
-         {
-            
-             var img = document.createElement("img");
-             img.src = data.data[i].images.fixed_height.url ;
-             imageTabsDiv.appendChild(img);
+      for (let i = 0; i < data.data.length; i++) {
+        const img = document.createElement('img');
 
-
-     }
-
+        img.src = data.data[i].images.fixed_height.url;
+        imageTabsDiv.appendChild(img);
+      }
     });
   });
-
-var $submitButton = $('#submit');
+}());
--- landingapp.js
+++ landingapp.js
@@ -1,47 +1,40 @@
-
-
-!function(a){var b="object"==typeof self&&self.self===self&&self||"object"==typeof global&&global.global===global&&global;"function"==typeof define&&define.amd?define(["exports"],function(c){b.ParticleNetwork=a(b,c)}):"object"==typeof module&&module.exports?module.exports=a(b,{}):b.ParticleNetwork=a(b,{})}(function(a,b){var c=function(a){this.canvas=a.canvas,this.g=a.g,this.particleColor=a.options.particleColor,this.x=Math.random()*this.canvas.width,this.y=Math.random()*this.canvas.height,this.velocity={x:(Math.random()-.5)*a.options.velocity,y:(Math.random()-.5)*a.options.velocity}};return c.prototype.update=function(){(this.x>this.canvas.width+20||this.x<-20)&&(this.velocity.x=-this.velocity.x),(this.y>this.canvas.height+20||this.y<-20)&&(this.velocity.y=-this.velocity.y),this.x+=this.velocity.x,this.y+=this.velocity.y},c.prototype.h=function(){this.g.beginPath(),this.g.fillStyle=this.particleColor,this.g.globalAlpha=.7,this.g.arc(this.x,this.y,1.5,0,2*Math.PI),this.g.fill()},b=function(a,b){this.i=a,this.i.size={width:this.i.offsetWidth,height:this.i.offsetHeight},b=void 0!==b?b:{},this.options={particleColor:void 0!==b.particleColor?b.particleColor:"#fff",background:void 0!==b.background?b.background:"#1a252f",interactive:void 0!==b.interactive?b.interactive:!0,velocity:this.setVelocity(b.speed),density:this.j(b.density)},this.init()},b.prototype.init=function(){if(this.k=document.createElement("div"),this.i.appendChild(this.k),this.l(this.k,{position:"absolute",top:0,left:0,bottom:0,right:0,"z-index":1}),/(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)/i.test(this.options.background))this.l(this.k,{background:this.options.background});else{if(!/\.(gif|jpg|jpeg|tiff|png)$/i.test(this.options.background))return console.error("Please specify a valid background image or hexadecimal color"),!1;this.l(this.k,{background:'url("'+this.options.background+'") no-repeat center',"background-size":"cover"})}if(!/(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)/i.test(this.options.particleColor))return console.error("Please specify a valid particleColor hexadecimal color"),!1;this.canvas=document.createElement("canvas"),this.i.appendChild(this.canvas),this.g=this.canvas.getContext("2d"),this.canvas.width=this.i.size.width,this.canvas.height=this.i.size.height,this.l(this.i,{position:"relative"}),this.l(this.canvas,{"z-index":"20",position:"relative"}),window.addEventListener("resize",function(){return this.i.offsetWidth===this.i.size.width&&this.i.offsetHeight===this.i.size.height?!1:(this.canvas.width=this.i.size.width=this.i.offsetWidth,this.canvas.height=this.i.size.height=this.i.offsetHeight,clearTimeout(this.m),void(this.m=setTimeout(function(){this.o=[];for(var a=0;a<this.canvas.width*this.canvas.height/this.options.density;a++)this.o.push(new c(this));this.options.interactive&&this.o.push(this.p),requestAnimationFrame(this.update.bind(this))}.bind(this),500)))}.bind(this)),this.o=[];for(var a=0;a<this.canvas.width*this.canvas.height/this.options.density;a++)this.o.push(new c(this));this.options.interactive&&(this.p=new c(this),this.p.velocity={x:0,y:0},this.o.push(this.p),this.canvas.addEventListener("mousemove",function(a){this.p.x=a.clientX-this.canvas.offsetLeft,this.p.y=a.clientY-this.canvas.offsetTop}.bind(this)),this.canvas.addEventListener("mouseup",function(a){this.p.velocity={x:(Math.random()-.5)*this.options.velocity,y:(Math.random()-.5)*this.options.velocity},this.p=new c(this),this.p.velocity={x:0,y:0},this.o.push(this.p)}.bind(this))),requestAnimationFrame(this.update.bind(this))},b.prototype.update=function(){this.g.clearRect(0,0,this.canvas.width,this.canvas.height),this.g.globalAlpha=1;for(var a=0;a<this.o.length;a++){this.o[a].update(),this.o[a].h();for(var b=this.o.length-1;b>a;b--){var c=Math.sqrt(Math.pow(this.o[a].x-this.o[b].x,2)+Math.pow(this.o[a].y-this.o[b].y,2));c>120||(this.g.beginPath(),this.g.strokeStyle=this.options.particleColor,this.g.globalAlpha=(120-c)/120,this.g.lineWidth=.7,this.g.moveTo(this.o[a].x,this.o[a].y),this.g.lineTo(this.o[b].x,this.o[b].y),this.g.stroke())}}0!==this.options.velocity&&requestAnimationFrame(this.update.bind(this))},b.prototype.setVelocity=function(a){return"fast"===a?1:"slow"===a?.33:"none"===a?0:.66},b.prototype.j=function(a){return"high"===a?5e3:"low"===a?2e4:isNaN(parseInt(a,10))?1e4:a},b.prototype.l=function(a,b){for(var c in b)a.style[c]=b[c]},b});
-
-// Initialisation
-
-var canvasDiv = document.getElementById('particle-canvas');
-var options = {
-  particleColor: '#A239CA',
-  background: '#000000',
-  interactive: true,
-  speed: 'high',
-  density: 'high'
+(function() {
+  'use strict';
+  !function(a){let b="object"==typeof self&&self.self===self&&self||"object"==typeof global&&global.global===global&&global;"function"==typeof define&&define.amd?define(["exports"],function(c){b.ParticleNetwork=a(b,c)}):"object"==typeof module&&module.exports?module.exports=a(b,{}):b.ParticleNetwork=a(b,{})}(function(a,b){let c=function(a){this.canvas=a.canvas,this.g=a.g,this.particleColor=a.options.particleColor,this.x=Math.random()*this.canvas.width,this.y=Math.random()*this.canvas.height,this.velocity={x:(Math.random()-.5)*a.options.velocity,y:(Math.random()-.5)*a.options.velocity}};return c.prototype.update=function(){(this.x>this.canvas.width+20||this.x<-20)&&(this.velocity.x=-this.velocity.x),(this.y>this.canvas.height+20||this.y<-20)&&(this.velocity.y=-this.velocity.y),this.x+=this.velocity.x,this.y+=this.velocity.y},c.prototype.h=function(){this.g.beginPath(),this.g.fillStyle=this.particleColor,this.g.globalAlpha=.7,this.g.arc(this.x,this.y,1.5,0,2*Math.PI),this.g.fill()},b=function(a,b){this.i=a,this.i.size={width:this.i.offsetWidth,height:this.i.offsetHeight},b=void 0!==b?b:{},this.options={particleColor:void 0!==b.particleColor?b.particleColor:"#fff",background:void 0!==b.background?b.background:"#1a252f",interactive:void 0!==b.interactive?b.interactive:!0,velocity:this.setVelocity(b.speed),density:this.j(b.density)},this.init()},b.prototype.init=function(){if(this.k=document.createElement("div"),this.i.appendChild(this.k),this.l(this.k,{position:"absolute",top:0,left:0,bottom:0,right:0,"z-index":1}),/(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)/i.test(this.options.background))this.l(this.k,{background:this.options.background});else{if(!/\.(gif|jpg|jpeg|tiff|png)$/i.test(this.options.background))return console.error("Please specify a valid background image or hexadecimal color"),!1;this.l(this.k,{background:'url("'+this.options.background+'") no-repeat center',"background-size":"cover"})}if(!/(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)/i.test(this.options.particleColor))return console.error("Please specify a valid particleColor hexadecimal color"),!1;this.canvas=document.createElement("canvas"),this.i.appendChild(this.canvas),this.g=this.canvas.getContext("2d"),this.canvas.width=this.i.size.width,this.canvas.height=this.i.size.height,this.l(this.i,{position:"relative"}),this.l(this.canvas,{"z-index":"20",position:"relative"}),window.addEventListener("resize",function(){return this.i.offsetWidth===this.i.size.width&&this.i.offsetHeight===this.i.size.height?!1:(this.canvas.width=this.i.size.width=this.i.offsetWidth,this.canvas.height=this.i.size.height=this.i.offsetHeight,clearTimeout(this.m),void(this.m=setTimeout(function(){this.o=[];for(let a=0;a<this.canvas.width*this.canvas.height/this.options.density;a++)this.o.push(new c(this));this.options.interactive&&this.o.push(this.p),requestAnimationFrame(this.update.bind(this))}.bind(this),500)))}.bind(this)),this.o=[];for(let a=0;a<this.canvas.width*this.canvas.height/this.options.density;a++)this.o.push(new c(this));this.options.interactive&&(this.p=new c(this),this.p.velocity={x:0,y:0},this.o.push(this.p),this.canvas.addEventListener("mousemove",function(a){this.p.x=a.clientX-this.canvas.offsetLeft,this.p.y=a.clientY-this.canvas.offsetTop}.bind(this)),this.canvas.addEventListener("mouseup",function(a){this.p.velocity={x:(Math.random()-.5)*this.options.velocity,y:(Math.random()-.5)*this.options.velocity},this.p=new c(this),this.p.velocity={x:0,y:0},this.o.push(this.p)}.bind(this))),requestAnimationFrame(this.update.bind(this))},b.prototype.update=function(){this.g.clearRect(0,0,this.canvas.width,this.canvas.height),this.g.globalAlpha=1;for(let a=0;a<this.o.length;a++){this.o[a].update(),this.o[a].h();for(let b=this.o.length-1;b>a;b--){let c=Math.sqrt(Math.pow(this.o[a].x-this.o[b].x,2)+Math.pow(this.o[a].y-this.o[b].y,2));c>120||(this.g.beginPath(),this.g.strokeStyle=this.options.particleColor,this.g.globalAlpha=(120-c)/120,this.g.lineWidth=.7,this.g.moveTo(this.o[a].x,this.o[a].y),this.g.lineTo(this.o[b].x,this.o[b].y),this.g.stroke())}}0!==this.options.velocity&&requestAnimationFrame(this.update.bind(this))},b.prototype.setVelocity=function(a){return"fast"===a?1:"slow"===a?.33:"none"===a?0:.66},b.prototype.j=function(a){return"high"===a?5e3:"low"===a?2e4:isNaN(parseInt(a,10))?1e4:a},b.prototype.l=function(a,b){for(let c in b)a.style[c]=b[c]},b});
+  const canvasDiv = document.getElementById('particle-canvas');
+  const options = {
+    particleColor: '#A239CA',
+    background: '#000000',
+    interactive: true,
+    speed: 'high',
+    density: 'high'
 };
-var particleCanvas = new ParticleNetwork(canvasDiv, options);
-SC.initialize({
-  client_id: '8b8c9f6cb9ff0c2479d1c1ead48eefd2'
+const particleCanvas = new ParticleNetwork(canvasDiv, options);
+  SC.initialize( {
+    client_id: '8b8c9f6cb9ff0c2479d1c1ead48eefd2'
 });
-
-var $submitButton = $('#artist');
-var id;
-var sondid;
-
-
+const $submitButton = $('#artist');
 $('#submit').click(function(event) {
+  let id;
+
+  let sondid;
+
   event.preventDefault();
-  var s = $submitButton.val().split(" ");
+
+  const s = $submitButton.val().split(' ');
 
   SC.get('/users', {
     q:s[0] , username:s[0]
   }).then(function(tracks) {
-
     id=tracks[0].id;
     return SC.get('/tracks', {
       q:s[1], user_id: id
     });
-
-  })
-.then(function(tracks) {
+  }).then(function(tracks) {
   sondid = tracks[0].id;
-  var $xhr = $.getJSON(`https://cors-anywhere.herokuapp.com/http://api.soundcloud.com/resolve?url=https://api.soundcloud.com/tracks/${sondid}&client_id=8b8c9f6cb9ff0c2479d1c1ead48eefd2`);
+  const $xhr = $.getJSON(`https://cors-anywhere.herokuapp.com/http://api.soundcloud.com/resolve?url=https://api.soundcloud.com/tracks/${sondid}&client_id=8b8c9f6cb9ff0c2479d1c1ead48eefd2`);
 
   $xhr.done(function(track) {
-    var count = 0;
     SC.oEmbed(track.permalink_url, {
       element: document.getElementById('target')
     });
@@ -49,10 +42,8 @@ $('#submit').click(function(event) {
 
   $xhr.fail(function() {
     if ($xhr.status === 403) {
-      console.log('Hello');
     }
-
+  });
   });
 });
-
-});
+}());
